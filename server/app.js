650c6588ef34f67c1b3293be6e3b24ae3fee86d5
jax <284973314@qq.com>
2016-11-20 17:13:54 +0800

    init-html-template

--- server/app.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by jax.luo on 2016/11/18.
- */
-app.use(function (req, res, next) {
-    var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    console.log('Client IP:', ip);
-    next();
-});
--- server/models/user.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by jax.luo on 2016/11/18.
- */
-module.exports = function (account) {
-    this.account = account;
-    this.nickname = account;
-    this.avatarUrl = '';
-};
